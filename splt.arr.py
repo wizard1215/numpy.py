9db0aa876525c81e5e21be036bec8f3cee5cc894
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-06-06 11:15:47 +0530

    Add files via upload

---
+++ splt.arr.py
@@ -0,0 +1,42 @@
+# There are three to split array:
+# 1) np.split() it split the array into the equally parts:
+#2) np.array_split() it split array if not equally:
+
+"""import numpy as np
+array=np.array([10,20,30,40,50,60])
+result=np.split(array,2)
+print(result) """
+
+
+"""import numpy as np
+a=np.array([10,20,30,40,50])
+print(np.split(a[2,4]))"""
+
+
+# example of np.array_split
+"""import numpy as np
+a1=np.array([10,20,30,40,50])
+new_array=np.array_split(a1)
+print(new_array)"""
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
