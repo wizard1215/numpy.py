8abb3e09e2147c78760f5303e60c65da65a99163
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-06-06 11:20:21 +0530

    Add files via upload

---
+++ numpy_dstack.py
@@ -0,0 +1,42 @@
+# It is covert 2d array into 3d array: here should be same row and column: 
+"""import numpy as np
+array1=np.array([10,20,30,40])
+array2=np.array([50,60,70,80])
+print(np.dstack((array1,array2)))"""
+
+
+"""import numpy as np
+a=np.array([[1,2],
+            [3,4]])
+b=np.array([[5,6],
+            [7,8]])
+print(np.dstack((a,b)))"""
+
+"""import numpy as np
+a=np.array([[1,2]])
+b=np.array([[3,4]])
+c=np.array([[5,6]])
+new_array=(np.dstack((a,b,c)))
+print(new_array.ndim)"""
+
+#find the shape of these arrays:
+"""import numpy as np
+x=np.array([10,20,30])
+y=np.array([40,50,60])
+new_array=np.dstack((x,y))
+print(new_array)
+print(new_array.shape)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
